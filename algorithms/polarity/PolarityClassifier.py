9e8fe3d21846c622adcf63e24d96701b51e346a2
Priya Ananthasankar <panantha@Priyas-MacBook-Pro.local>
2016-11-25 16:07:31 -0800

    polarity classifier

--- algorithms/polarity/PolarityClassifier.py
+++ algorithms/polarity/PolarityClassifier.py
@@ -1,6 +1,13 @@
 import nltk
 import sys
 import csv
+from nltk import ngrams
+import collections
+from nltk.metrics.scores import   (accuracy, precision, recall, f_measure,
+                                          log_likelihood, approxrand)
+from nltk import precision
+from textblob import TextBlob
+
 train_data = sys.argv[1]
 test_data = sys.argv[2]
 
@@ -18,45 +25,94 @@ sn =0
 nsp = 0
 nsn = 0
 
-def pol_stats(dir):
+
+classified = collections.defaultdict(set)
+observed = collections.defaultdict(set)
+correct_tags = 0
+incorrect_tags = 0
+
+def polarizer(dir):
     with open(dir, 'r',encoding='utf-8', errors='ignore') as csvfile:
         reader = csv.reader(csvfile)
+        i = 0
+        global correct_tags, incorrect_tags
         for original_tweet,text,hashtags,users,length,label in reader:
             words = text.split()
-            positive_per_tweet = []
-            negative_per_tweet = []
+            seed_word = ""
+            isSeedWordPositive = False
+
             for word in words:
                 if word in positive_words:
-                    positive_per_tweet.append(word)
-                    if label == "S":
-                        sp += 1
-                    if label == "NS":
-                        sn += 1
-                if word in negative_words:
-                    negative_per_tweet.append(word)
-                    if label == "S":
-                        nsp += 1
-                    if label == "NS":
-                        nsn += 1
+                    seed_word = word
+                    isSeedWordPositive = True
+                    prefix_suffix = text.partition(seed_word)
+                    break
+                elif word in negative_words:
+                    seed_word = word
+                    prefix_suffix = text.partition(seed_word)
+                    break
 
-pol_stats(train_data)
-print ("TRAINING DATA :")
-print ("Sarcastic Positive: " , sp)
-print ("Sarcastic Negative" , sn)
+            if seed_word:
+                if isSeedWordPositive:
+                    # if you got a positive seed word, then take all the bigram prefix and suffix- look for a negative score
+                    prefix = prefix_suffix[0]
+                    suffix = prefix_suffix[2]
+                else:
+                    # if you got a negative seed, then take all the bigram prefix and suffix - look for a positive score
+                    prefix = prefix_suffix[0]
+                    suffix = prefix_suffix[2]
 
-print ("Non Sarcastic Positive: " , nsp)
-print ("Non Sarcastic Negative" , nsn)
+                bi_gram_prefix = ngrams(prefix.split(), 2)
+                bi_gram_suffix = ngrams(suffix.split(), 2)
 
-sp = 0
-sn =0
-nsp = 0
-nsn = 0
+                pol_prefix = 0.0
+                pol_suffix = 0.0
 
-pol_stats(test_data)
+                for bg in bi_gram_prefix:
+                    blob = TextBlob(" ".join(bg))
+                    pol_prefix += blob.sentiment.polarity
 
-print ("TESTING DATA :")
-print ("Sarcastic Positive: " , sp)
-print ("Sarcastic Negative" , sn)
+                for bg in bi_gram_suffix:
+                    blob = TextBlob(" ".join(bg))
+                    pol_suffix += blob.sentiment.polarity
 
-print ("Non Sarcastic Positive: " , nsp)
-print ("Non Sarcastic Negative" , nsn)
+                if (pol_prefix + pol_suffix) > 0 and isSeedWordPositive == False:
+                    c_label = "S"
+                elif (pol_prefix + pol_suffix) < 0 and isSeedWordPositive:
+                    c_label = "S"
+                else:
+                    c_label = "NS"
+
+            else:
+                blob = TextBlob(text)
+                polarity = blob.sentiment.polarity
+
+                if polarity < 0 :
+                    c_label = "S"
+                else:
+                    c_label = "NS"
+
+            observed[label].add(i)
+            classified[c_label].add(i)
+            if c_label == label:
+                correct_tags += 1
+            else:
+                incorrect_tags += 1
+
+            i += 1
+                #print("POLARITY_PREFIX: ", pol_prefix)
+                #print("POLARITY_SUFFIX: ", pol_suffix)
+                #print("SEED WORD:",seed_word, "POLARITY", isSeedWordPositive)
+                #print("PREFIX:",prefix)
+                #print("SUFFIX:",suffix)
+
+polarizer(train_data)
+polarizer(test_data)
+
+print("Accuracy: ", correct_tags/(correct_tags + incorrect_tags))
+print('Sarcasm precision:', precision(observed["S"], classified["S"]))
+print('Sarcasm recall:', recall(observed['S'], classified['S']))
+print('Sarcasm F-measure:', f_measure(observed['S'], classified['S']))
+print('Not Sarcasm precision:',precision(observed['NS'], classified['NS']))
+print('Not Sarcasm recall:', recall(observed['S'], classified['NS']))
+print('Not Sarcasm F-measure:', f_measure(observed['S'], classified['NS']))
