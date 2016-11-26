73dd1c43e4c997baa29ef8918c1d9d3c09d7e863
Ravi Kiran Chadalawada <ravisrhyme@gmail.com>
2016-11-25 22:33:02 -0800

    Structured code to run multiple classifications,in each execution, with different set of features

--- algorithms/svm/scikit_svm.py
+++ algorithms/svm/scikit_svm.py
@@ -15,39 +15,56 @@ __status__  = "Prototype"
 
 
 
-import sys,csv
+import sys,csv,json
+sys.path.append('../polarity/')
 from sklearn import svm
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_recall_fscore_support
+from PolarityClassifier import get_polarity_per_tweet
+import nltk 
+import numpy as np
+from sklearn import preprocessing
 
 vec = DictVectorizer()
-data_list_dict =[]
-total_labels = []
-
 clf = svm.LinearSVC()
+metrics_list =[]
 
-def read_tweets_to_list(file_name):
+def read_tweets_to_list(file_name,data_list_dict,total_labels,feature_set):
 	fd = open(file_name,'r')
 	csv_reader = csv.reader(fd)
+
 	for row in csv_reader:
+		interjection = 0;
 		data_dict = {'tweet':None, 'length':None}
-		#print row
+
 		data_dict['tweet'] = row[0]
 		data_dict['length'] = row[4]
+
+		if 'interjection' in feature_set:
+			tokens = nltk.word_tokenize(row[0])
+			pos_tuple = nltk.pos_tag(tokens)
+			for pos in pos_tuple:
+				word,ptag = pos
+				if ptag == 'UH':
+					interjection += 1
+			data_dict['interjection'] = interjection
+
+		if 'polarity' in feature_set:
+			data_dict['ploarity'] = get_polarity_per_tweet(row[1])
+
 		data_list_dict.append(data_dict)
 		if row[5] == 'S':
 			total_labels.append(1)
 		else :
 			total_labels.append(-1)
-		#print row[0] + " " + row[4] + " " + row[5]
-
+		#print(data_list_dict)
+	fd.close()
 	return
 
 def list_to_features(data_list_dict):
 	x_features = [];
 	x_features = vec.fit_transform(data_list_dict).toarray()
-        #print x_features
         #print vec.get_feature_names()
 	return x_features;
 
@@ -57,31 +74,89 @@ def train_svm(training_features,training_labels):
 	return
 
 
-def test_svm(testing_features,actual_labels):
+def test_svm(testing_features,actual_labels,feature_instance):
 	
 	predicted_labels = []
+	metrics = {}
+
 	for feature_set in testing_features:
 		predicted_labels.extend(clf.predict([feature_set]))
-	#print predicted_labels
-	print accuracy_score(actual_labels,predicted_labels)
-	print precision_recall_fscore_support(actual_labels,predicted_labels, average=None, labels=['1','-1'])
+	#print(len(predicted_labels))
+	#print(len(actual_labels))
+	accuracy = accuracy_score(actual_labels,predicted_labels)
+	metrics_array = precision_recall_fscore_support(actual_labels,predicted_labels, average=None, labels=['1','-1'])
+	print(metrics_array)
+	
+	if feature_instance == 1:
+		metrics['feature_type'] = 'feature_basic'
+	elif feature_instance == 2:
+		metrics['feature_type'] = 'feature_intj'
+	else:
+		metrics['feature_type'] = 'feature_intjploar'
+
+	metrics["accuracy"] = accuracy
+	metrics["sarcasm_precision"] = metrics_array[0][0]
+	metrics["sarcasm_recall"] = metrics_array[1][0]
+	metrics["sarcasm_f_measure"] = metrics_array[2][0]
+	metrics["not_sarcasm_precision"] = metrics_array[0][1]
+	metrics["not_sarcasm_recall"] = metrics_array[1][1]
+	metrics["not_sarcasm_f_measure"] = metrics_array[2][1]
+	
+	metrics_list.append(metrics)
 	return
 
 
 if __name__ == '__main__':
-	read_tweets_to_list(sys.argv[1])
-	read_tweets_to_list(sys.argv[2])
+
+	data_list_dict = []
+	total_labels = []
+
+	#1
+	feature_set = []
+	read_tweets_to_list(sys.argv[1],data_list_dict,total_labels,feature_set)
+	read_tweets_to_list(sys.argv[2],data_list_dict,total_labels,feature_set)
 	total_features = list_to_features(data_list_dict)
-	#print len(total_features[0])
 	
 	training_features = total_features[:2250]
 	training_labels = total_labels[:2250]
 	train_svm(training_features,training_labels)
 	
 	testing_features = total_features[2250:]
-        testing_labels = total_labels[2250:]
-	test_svm(testing_features,testing_labels)
+	testing_labels = total_labels[2250:]
+	test_svm(testing_features,testing_labels,1)
+	
+	#2	
+	feature_set = ['interjection']
+	del data_list_dict[:]
+	del total_labels[:]
+	read_tweets_to_list(sys.argv[1],data_list_dict,total_labels,feature_set)
+	read_tweets_to_list(sys.argv[2],data_list_dict,total_labels,feature_set)
+	total_features = list_to_features(data_list_dict)
 
-	#array = vec.fit_transform(data).toarray()
-	#print array
-	#print vec.get_feature_names()
+	training_features = total_features[:2250]
+	training_labels = total_labels[:2250]
+	train_svm(training_features,training_labels)
+
+	testing_features = total_features[2250:]
+	testing_labels = total_labels[2250:]
+	test_svm(testing_features,testing_labels,2)
+
+        #3
+	feature_set = ['interjection','polarity']
+	del data_list_dict[:]
+	del total_labels[:]
+	read_tweets_to_list(sys.argv[1],data_list_dict,total_labels,feature_set)
+	read_tweets_to_list(sys.argv[2],data_list_dict,total_labels,feature_set)
+	total_features = list_to_features(data_list_dict)
+
+	training_features = total_features[:2250]
+	training_labels = total_labels[:2250]
+	train_svm(training_features,training_labels)
+
+	testing_features = total_features[2250:]
+	testing_labels = total_labels[2250:]
+	test_svm(testing_features,testing_labels,3)
+
+	print("\n\n")
+	json_data = json.dumps(metrics_list)
+	print(json_data)
